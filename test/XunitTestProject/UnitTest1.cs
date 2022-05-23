42a39a3144ed00cdd2c69809315472474ccb9a34
barbarosyurttagul <barbaros.yurttagul@gmail.com>
2022-05-23 19:05:56 +0300

    Test Project created

---
+++ test/XunitTestProject/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccessLayer.Concrete;
+using Entities;
+using Xunit;
+
+namespace XunitTestProject;
+
+public class UnitTest1
+{
+    [Fact]
+    public void ShouldDeparturesEqualToGivenNumber()
+    {
+        IDepartureServ service = new DepartureImpl(new DepartureDAL());
+        service.Add(new Departure { Id = 4, Name = "Manavgat" });
+
+        Assert.Equal(4, service.GetAll().Count);
+    }
+}

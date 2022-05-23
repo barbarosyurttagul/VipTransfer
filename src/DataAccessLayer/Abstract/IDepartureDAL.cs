0c2518d7202fb7603481026e85c3b7c6944e2614
barbarosyurttagul <barbaros.yurttagul@gmail.com>
2022-05-23 12:37:41 +0300

    IDepartureDAL created

---
+++ src/DataAccessLayer/Abstract/IDepartureDAL.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Entities;
+
+namespace DataAccessLayer.Abstract
+{
+	public interface IDepartureDAL
+	{
+		public void Add(string name);
+
+		public List<Departure> GetAll();
+	}
+}
+

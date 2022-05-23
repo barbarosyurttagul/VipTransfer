fc696185321859af626ed7068cb7c68fb3c19b73
barbarosyurttagul <barbaros.yurttagul@gmail.com>
2022-05-24 00:40:56 +0300

    WebAPI layer created

---
+++ src/VipTransfer.WebAPI/Controllers/DepartureController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccessLayer.Concrete;
+using Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VipTransfer.WebAPI.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class DepartureController
+	{
+        IDepartureServ service = new DepartureImpl(new DepartureDAL());
+
+        [HttpGet(Name = "GetDepartures")]
+        public IEnumerable<Departure> Get()
+        {
+            return service.GetAll();
+        }
+    }
+}
+

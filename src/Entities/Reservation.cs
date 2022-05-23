b86c7c5a810265047607bddb47e4d9fabcaa24a0
barbarosyurttagul <barbaros.yurttagul@gmail.com>
2022-05-23 12:24:06 +0300

    Reservation Class Added

---
+++ src/Entities/Reservation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Entities
+{
+	public class Reservation
+	{
+        public int Id { get; set; }
+        public Departure Departure { get; set; }
+        public Arrival Arrival { get; set; }
+        public DateTime DepartureTime { get; set; }
+        public string? FlightNumber { get; set; }
+    }
+}
+
